c4ded61c4441a9e0ea48901f59be66494acc96a9
vagrant <vagrant@vagrant.vm>
2020-10-22 20:01:41 +0000

    continued on weather

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -14,25 +14,30 @@
 
 function showFortune(evt) {
     $.get('/fortune', (response) => {
-        $('#fortune-text').html(response);
+    $('#fortune-text').html(response);
     })}
     // TODO: get the fortune and show it in the #fortune-text div
 
 $('#get-fortune-button').on('click', showFortune);
 
-
-
-
-
+// $('#update-status').on('click', () => {
+//     // Our GET request URL will look like this:
+//     //       /status?order=123
+//     $.get('/status', { order: 123 }, (res) => {
+//       $('#order-status').html(res);
+//     });
+//   });
 // PART 2: SHOW WEATHER
-
+// .get with arguements
 function showWeather(evt) {
     evt.preventDefault();
 
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
 
-
+    $.get(url, {formData}, (response) => {
+    $('#weather-info').html(response);
+    });
     // TODO: request weather with that URL and show the forecast in #weather-info
 }
 
